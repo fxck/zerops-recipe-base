82c54ac595c43da98fcfdf691559092471279d58
Ales Rechtorik <foxxcz@gmail.com>
2022-01-20 12:26:24 +0100

    diagram web element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ProjectDiagramModule } from './project-diagram/project-diagram.module';
+// import { ProjectDiagramModule } from './project-diagram/project-diagram.module';
 import { TodosModule } from './todos/todos.module';
 
 @NgModule({
@@ -10,9 +10,10 @@ import { TodosModule } from './todos/todos.module';
   imports: [
     BrowserModule,
     TodosModule,
-    ProjectDiagramModule,
+    // ProjectDiagramModule,
     HttpClientModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
